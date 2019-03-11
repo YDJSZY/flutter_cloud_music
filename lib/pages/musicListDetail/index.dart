702600b200cfedb4d3b4810274077386381853db
YDJSZY <260211963@qq.com>
2019-03-11 22:59:27 +0800

    commit

--- lib/pages/musicListDetail/index.dart
+++ lib/pages/musicListDetail/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import '../../apiRequest/index.dart';
 
 class MusicListDetail extends StatefulWidget {
@@ -58,6 +59,7 @@ class _MusicListDetail extends State<MusicListDetail> {
 
   @override
   Widget build(BuildContext context) {
+    var coverImgUrl = songList['playlist']['coverImgUrl'];
     var avatarUrl = songList['playlist']['creator']['avatarUrl'];
     var avatarName = songList['playlist']['creator']['nickname'];
     var musicCount = songList['playlist']['tracks'].length;
@@ -65,41 +67,62 @@ class _MusicListDetail extends State<MusicListDetail> {
       appBar: AppBar(
         title: Text('歌单'),
         centerTitle: true,
+        backgroundColor: Colors.transparent,
       ),
       body: Container(
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(15.0),
-              child: Row(
+            Expanded(
+              child: Stack(
+                fit: StackFit.expand,
                 children: <Widget>[
-                  songList['playlist']['coverImgUrl'] != null ? Image.network(songList['playlist']['coverImgUrl'], width: 150.0, height: 150.0,) : Container(),
-                  Flexible( // 包装在Flexible控件中，使其填充Row主轴中的可用空间
-                    child: Container(
-                      padding: EdgeInsets.only(left: 15.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(songList['playlist']['name'], style: TextStyle(fontSize: 18.0), overflow: TextOverflow.clip,),
-                          avatarUrl != null ? Container(
-                            padding: EdgeInsets.only(top: 20.0),
-                            child: Row(
-                              children: <Widget>[
-                                Container(
-                                  width: 40.0,
-                                  height: 40.0,
-                                  margin: EdgeInsets.only(right: 10.0),
-                                  child: CircleAvatar( // 圆角头像
-                                    backgroundImage: NetworkImage(songList['playlist']['creator']['avatarUrl']),
-                                  ),
+                  coverImgUrl != null ? Image.network(
+                    coverImgUrl,
+                    fit: BoxFit.fitWidth,
+                  ) : Container(),
+                  BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: new Container(
+                      color: Colors.white.withOpacity(0.8),
+                      height: 200.0,
+                      child: Container(
+                        padding: EdgeInsets.all(15.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            coverImgUrl != null ? Image.network(songList['playlist']['coverImgUrl'], width: 150.0, height: 150.0,) : Container(),
+                            Flexible( // 包装在Flexible控件中，使其填充Row主轴中的可用空间
+                              child: Container(
+                                padding: EdgeInsets.only(left: 15.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(songList['playlist']['name'], style: TextStyle(fontSize: 18.0), overflow: TextOverflow.clip,),
+                                    avatarUrl != null ? Container(
+                                      padding: EdgeInsets.only(top: 20.0),
+                                      child: Row(
+                                        children: <Widget>[
+                                          Container(
+                                            width: 40.0,
+                                            height: 40.0,
+                                            margin: EdgeInsets.only(right: 10.0),
+                                            child: CircleAvatar( // 圆角头像
+                                              backgroundImage: NetworkImage(songList['playlist']['creator']['avatarUrl']),
+                                            ),
+                                          ),
+                                          Text(avatarName)
+                                        ],
+                                      )
+                                    ) : Container()
+                                  ],
                                 ),
-                                Text(avatarName)
-                              ],
+                              ),
                             )
-                          ) : Container()
-                        ],
-                      ),
+                          ],
+                        ),
+                      )
                     ),
                   )
                 ],
